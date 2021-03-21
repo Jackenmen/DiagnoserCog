8b80d59d8d20ecdc60383c544bef1a58b53542f6
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-03-21 19:23:28 +0100

    Add cog metadata

--- diagnoser/__init__.py
+++ diagnoser/__init__.py
@@ -2,6 +2,10 @@ from redbot.core.bot import Red
 
 from .diagnoser import Diagnoser
 
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
 
 def setup(bot: Red) -> None:
     cog = Diagnoser(bot)
