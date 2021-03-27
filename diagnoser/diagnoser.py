dc4c7914a917247eab5caaeddebea1bab9e56938
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-03-27 04:29:26 +0100

    Remove leading whitespace after newline in the new strings

--- diagnoser/diagnoser.py
+++ diagnoser/diagnoser.py
@@ -154,7 +154,7 @@ class DetailedGlobalCallOnceChecksMixin(IssueDiagnoserBase):
                 _(
                     "To fix this issue, you can either add the user to the allowlist,"
                     " or clear the allowlist.\n"
-                    " If you want to keep the allowlist, you can run {command_1} which will"
+                    "If you want to keep the allowlist, you can run {command_1} which will"
                     " add the {user} user to the allowlist.\n"
                     "If you instead want to clear the allowlist and let all users"
                     " run commands freely, you can run {command_2} to do that."
@@ -171,7 +171,7 @@ class DetailedGlobalCallOnceChecksMixin(IssueDiagnoserBase):
             _(
                 "To fix this issue, you can either remove the user from the blocklist,"
                 " or clear the blocklist.\n"
-                " If you want to keep the blocklist, you can run {command_1} which will"
+                "If you want to keep the blocklist, you can run {command_1} which will"
                 " remove the {user} user from the blocklist.\n"
                 "If you instead want to clear the blocklist and let all users"
                 " run commands freely, you can run {command_2} to do that."
@@ -194,7 +194,7 @@ class DetailedGlobalCallOnceChecksMixin(IssueDiagnoserBase):
                 _(
                     "To fix this issue, you can either add the user or one of their roles"
                     " to the local allowlist, or clear the local allowlist.\n"
-                    " If you want to keep the local allowlist, you can run {command_1} which will"
+                    "If you want to keep the local allowlist, you can run {command_1} which will"
                     " add the {user} user to the local allowlist.\n"
                     "If you instead want to clear the local allowlist and let all users"
                     " run commands freely, you can run {command_2} to do that."
@@ -223,7 +223,7 @@ class DetailedGlobalCallOnceChecksMixin(IssueDiagnoserBase):
                 _(
                     "To fix this issue, you can either remove the user's roles"
                     " from the local blocklist, or clear the local blocklist.\n"
-                    " If you want to keep the local blocklist, you can run {command_1} which will"
+                    "If you want to keep the local blocklist, you can run {command_1} which will"
                     " remove the user's roles ({roles}) from the local blocklist.\n"
                     "If you instead want to clear the local blocklist and let all users"
                     " run commands freely, you can run {command_2} to do that."
@@ -248,7 +248,7 @@ class DetailedGlobalCallOnceChecksMixin(IssueDiagnoserBase):
                 _(
                     "To fix this issue, you can either remove the user and their roles"
                     " from the local blocklist, or clear the local blocklist.\n"
-                    " If you want to keep the local blocklist, you can run {command_1} which will"
+                    "If you want to keep the local blocklist, you can run {command_1} which will"
                     " remove the {user} user and their roles ({roles}) from the local blocklist.\n"
                     "If you instead want to clear the local blocklist and let all users"
                     " run commands freely, you can run {command_2} to do that."
